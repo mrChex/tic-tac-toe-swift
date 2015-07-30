249563de1b52969a022c7c0e8025f10450b9bc90
Ruslan Cheshko <chex@soul.li>
2015-07-30 14:54:56 +0300

    Initial Commit

---
+++ tic-tac-toe/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  tic-tac-toe
+//
+//  Created by Ruslan Cheshko on 7/30/15.
+//  Copyright (c) 2015 Ukrainian.Solutions. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ tic-tac-toe/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  tic-tac-toe
+//
+//  Created by Ruslan Cheshko on 7/30/15.
+//  Copyright (c) 2015 Ukrainian.Solutions. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
